93ad5ff6ac3b213f2d2ce759b73686e65ffb1dc2
Klavikus <912654q@gmail.com>
2024-04-02 09:18:24 +0300

    Step 3 (raw)
    (cherry picked from commit e0ad1f92aa303be1eaddd2da2121d22bd7f75012)

--- Assets/Scripts/Creeps/Creep.cs
+++ Assets/Scripts/Creeps/Creep.cs
@@ -10,11 +10,9 @@ namespace Creeps
 
         private int _waypointIndex;
 
-        private void Update()
-        {
-            Move();
-        }
-
+        public LayerMask enemyLayer; // Используйте LayerMask для определения слоя врагов
+        public float attackRange = 5f; // Радиус обнаружения врага
+        
         public void Initialize(CreepStats stats, Transform[] waypoints)
         {
             _stats = stats;
@@ -28,6 +26,12 @@ namespace Creeps
             transform.position = _waypoints[_waypointIndex].transform.position;
         }
 
+        void Update()
+        {
+            Move();
+            DetectEnemies();
+        }
+
         private void Move()
         {
             if (_waypointIndex > _waypoints.Length - 1)
@@ -38,6 +42,37 @@ namespace Creeps
 
             if (transform.position == _waypoints[_waypointIndex].transform.position)
                 _waypointIndex += 1;
+        }
+
+        private void DetectEnemies()
+        {
+            // Простейшее обнаружение врага в пределах диапазона
+            Collider[] hitEnemies = Physics.OverlapSphere(transform.position, attackRange, enemyLayer);
+
+            foreach (var enemy in hitEnemies)
+            {
+                // Проверяем, соответствует ли team врага
+                Creep enemyCreep = enemy.GetComponent<Creep>();
+                if (enemyCreep != null && enemyCreep.stats.team != this.stats.team)
+                {
+                    Attack(enemyCreep);
+                    break; // Предполагаем атаку только одного врага для упрощения
+                }
+            }
+        }
+
+        private void Attack(Creep targetCreep)
+        {
+            // Здесь логика атаки или урон врагу
+            Debug.Log($"Attacking {targetCreep.name}");
+            // Например, можно вызвать метод получения урона у врага
+        }
+
+        // Вспомогательная функция для отображения области обнаружения врагов в редакторе
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackRange);
         }
     }
 }
